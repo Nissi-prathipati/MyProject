2f16a7c1d360531cb34c295a12cd42ccb91d5f97
Nissi-prathipati <140831771+Nissi-prathipati@users.noreply.github.com>
2023-08-08 09:57:06 +0000

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+const routes: Routes = [
+{path:'abc',component:DashboardComponent},
+{path:'cba',component:LoginComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
